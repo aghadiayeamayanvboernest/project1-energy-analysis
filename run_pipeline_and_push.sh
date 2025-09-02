b3be38052098d6ba6179978427455afdf303d6f8
Aghadiaye Ernest <ernestjacob789@gmail.com>
2025-09-02 13:09:45 -0700

    update

--- run_pipeline_and_push.sh
+++ run_pipeline_and_push.sh
@@ -9,7 +9,7 @@ git add data/
 
 # Only commit & push if there are changes
 if ! git diff --cached --quiet; then
-    git commit -m "Daily data update: $(date '+%Y-%m-%d %H:%M:%S')"
+    git commit -m "weekly data update: $(date '+%Y-%m-%d %H:%M:%S')"
     git push origin main
 else
     echo "No changes to commit."
